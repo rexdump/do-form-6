72c58afe97f400d24c6d33d3969b34ca22535bf6
Thomas Skerbis <skerbis@klxm.de>
2016-01-17 22:31:26 +0100

    Update contact-output.php

--- contact-output.php
+++ contact-output.php
@@ -9,7 +9,7 @@ echo "Anker: #doformREX_SLICE_ID";
 /**==================================================
  * REDAXO-Modul: contact!  http://klxm.de/produkte/
  * Bereich: Ausgabe
- * Version: 6.0, Datum: 17.01.2016
+ * Version: 1.0, Datum: 17.01.2016
  *==================================================*/
 //   KONFIGURATION
 $form_tag_class 	         = 'formgen'; // CSS Klasse des FORM-Tags
