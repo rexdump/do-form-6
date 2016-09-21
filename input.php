bf7fd25a610de276e076573e3d50f27efe46bc12
Thomas Skerbis <skerbis@klxm.de>
2016-09-21 14:20:34 +0200

    Update input.php

--- input.php
+++ input.php
@@ -4,7 +4,7 @@
  * REDAXO-Modul: contact! aka do form!
  * Bereich: Eingabe 
  */
-$doformversion="6.0.9";
+$doformversion="6.0.10";
  /**
  * ab Redaxo Version: 5
  * Werbeagentur KLXM Crossmedia  
